f2fddfca221223ba22be8ff34aa95d3abd8e5b18
Daniel Medeiros <danielmmf@gmail.com>
2017-07-18 18:49:02 -0300

    imagem

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -168,4 +168,26 @@ class ShopController extends Controller {
         return response()->json($resposta);
     }
 
+    public function imagem(){
+      $url = 'https://avon.vteximg.com.br/arquivos/ids/159197-400-400/advance-techniques-stiling-creme-de-efeito-liso-150g-avon-avn2428.jpg';
+      echo getcwd();
+      $this->grab_image($url, '.');
+    }
+
+
+    public function grab_image($url,$saveto){
+      $ch = curl_init ($url);
+      curl_setopt($ch, CURLOPT_HEADER, 0);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+      $raw=curl_exec($ch);
+      curl_close ($ch);
+      if(file_exists($saveto)){
+          unlink($saveto);
+      }
+      $fp = fopen($saveto,'x');
+      fwrite($fp, $raw);
+      fclose($fp);
+  }
+
 }
